509257f4eea03b1dfdd1b29877c232b73796db18
Riesenkrabbe <57005314+Riesenkrabbe@users.noreply.github.com>
2019-11-02 13:17:05 +0100

    Initial commit

---
+++ Assets/Scripts/PointSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour
+{
+    public int points = 0;
+    public int combo = 0;
+    private float combotimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Physics.IgnoreCollision(GameObject.Find("Player").GetComponent<Collider>(), GetComponent<Collider>());
+        Debug.Log("PointSystem Startup");
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        combo = 0;
+        combotimer = 0.0f;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        combo++;
+        combotimer = 0.0f;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        combotimer += Time.deltaTime;
+        if (combotimer >= 5.0f)
+        {
+            points += combo;
+            combo = 0;
+        }
+    }
+}
